6c8c57a326e92010308a2c13ed1d96ee77cbef32
Vitor <vitormd97@gmail.com>
2016-11-03 13:00:20 -0200

    Class Prova Updated

--- prjPOODeboraVitorLuiz/src/fatec/poo/model/Prova.java
+++ prjPOODeboraVitorLuiz/src/fatec/poo/model/Prova.java
@@ -7,21 +7,23 @@ public class Prova{
   private Questao[] questoes;
   private Concurso concurso;
   private Candidato candidato;
-  
+
+  int questoesAdd = 0;
+
   public Prova(String materia, int qtdeQuestoes) {
 	  this.materia = materia;
 	  this.qtdeQuestoes = qtdeQuestoes;
 	  questoes = new Questao[qtdeQuestoes];
   }
-  
+
   public void setPeso(int peso) {
 	  this.peso = peso;
   }
-  
+
   public void setConcurso(Concurso concurso) {
 	  this.concurso = concurso;
   }
-  
+
   public void setCandidato(Candidato candidato) {
 	  this.candidato = candidato;
   }
@@ -29,37 +31,47 @@ public class Prova{
     public int getPeso() {
         return peso;
     }
-  
-  
+
+
   public Candidato getCandidato() {
 	  return candidato;
   }
-  
+
   public Concurso getConcurso() {
 	  return concurso;
   }
-  
+
   public String getMateria() {
 	  return materia;
   }
-  
+
   public double getNota() {
 	  return nota;
   }
-  
+
   public int getQtdeQuestoes() {
 	  return qtdeQuestoes;
   }
-  
+
   public void efetuarCorrecao() {
 	 int corretas = 0;
-	 
+
 	 for(Questao questao : questoes) {
 		 if(questao.alternativaGabarito == questao.resposta) {
 			 corretas += 1;
 		 }
 	 }
-	 
+
 	 nota = ((corretas * 100) / qtdeQuestoes) / 10;
   }
+
+  public void addQuestao(Questao q) {
+    if(questoesAdd < qtdeQuestoes) {
+      questoes[questoesAdd++] = q;
+    }
+    else {
+      System.out.println("Limite de questoes excedido");
+    }
+  }
+
 }
