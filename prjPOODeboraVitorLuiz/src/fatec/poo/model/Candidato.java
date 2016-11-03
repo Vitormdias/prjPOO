6505c58efc03e2187de09174ab1990e20d4dbef1
DEBORA KAORI HIRAI <0030481511010@lti.fatecso.edu.br>
2016-11-03 12:33:46 -0200

    Class Candidato added

---
+++ prjPOODeboraVitorLuiz/src/fatec/poo/model/Candidato.java
@@ -0,0 +1,39 @@
+
+package fatec.poo.model;
+
+import java.util.ArrayList;
+
+public class Candidato extends Pessoa {
+    private String inscricao;
+    private double media;
+    private ArrayList<Prova> provas;
+
+    public Candidato(String inscricao, String cpf, String nome, String endereco) {
+        super(cpf, nome, endereco);
+        this.inscricao = inscricao;
+        provas = new ArrayList<Prova>();
+    }
+
+    public String getInscricao() {
+        return inscricao;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+    
+    public void addProva(Prova p){
+        provas.add(p);
+        p.setCandidato(this);
+    }
+   
+    public void calcularMedia(){
+        double somaPeso =0, somaNota = 0;
+        for(Prova p: provas){
+            somaNota += (p.getNota() * p.getPeso());  
+            somaPeso += p.getPeso();
+        }    
+        media = somaNota/somaPeso;
+    }        
+            
+}
